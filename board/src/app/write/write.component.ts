147f3550f98b170e3a09d96ac68d8b8a40e3a47a
김효성 <hyo@wemakeprice.com>
2018-01-10 12:26:29 +0900

    글등록 폼

--- board/src/app/write/write.component.ts
+++ board/src/app/write/write.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { } from ''
 @Component({
   selector: 'app-write',
   templateUrl: './write.component.html',
   styleUrls: ['./write.component.css']
 })
+
+
+
 export class WriteComponent implements OnInit {
+  frm = {
+    title: '',
+    body: ''
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+
+  submit() {
+    console.log("sub", this.frm)
+  }
 }
